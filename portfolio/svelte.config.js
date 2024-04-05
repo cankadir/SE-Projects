580a67279b50dffc82664a257a9f845da2cf136e
Can Sucuoğlu <cansucuoglu@gmail.com>
2024-04-05 16:27:53 -0400

    updated config

--- portfolio/svelte.config.js
+++ portfolio/svelte.config.js
@@ -3,12 +3,14 @@ import adapter from '@sveltejs/adapter-static';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
+		paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        },
 		prerender: {
 			// use relative URLs similar to an anchor tag <a href="/test/1"></a>
 			// do not include group layout folders in the path such as /(group)/test/1
