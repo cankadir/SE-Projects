4c089546413bbcaf4cb9a017cf1b6f6ff3cd6fee
Can Sucuoğlu <cansucuoglu@gmail.com>
2024-04-10 17:03:40 -0400

    testing mpa

--- docs/_app/immutable/chunks/paths.B0uO8135.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1c784ts)==null?void 0:s.base)??"/SE-Projects";var t;const a=((t=globalThis.__sveltekit_1c784ts)==null?void 0:t.assets)??e;export{a,e as b};
--- src/routes/projects/[id]/+page.svelte
+++ src/routes/projects/[id]/+page.svelte
@@ -1,22 +1,38 @@
 
 <script>
+    // @ts-nocheck
     import {page} from '$app/stores';
     import SplideCarousel from '$lib/components/component_slider.svelte'
 
-    
-    let filtered_data;
-    
-    // get data from main page which is PORHECTS_DATA after it loads
-    import PROJECTS_DATA from "$lib/data";
-    // filter data to get the project with the id that matches the id in the url
-    PROJECTS_DATA.then(data => {
-        filtered_data = data.filter(project => project.short_url === $page.params.id );
-        filtered_data = filtered_data[0];
+    import { onMount } from 'svelte';
+    let page_data = {};
+    onMount(() => {
+	    console.log('page is mounted');
+	    console.log(`url is ${$page.url}`);
+	    let paramstr = JSON.stringify($page.params);
+	    console.log(`params are ${paramstr}`);
+	    page_data = {
+		    title: $page.params.slug,
+	    };
     });
 
+    // let filtered_data;
+    
+    // // get data from main page which is PORHECTS_DATA after it loads
+    // import PROJECTS_DATA from "$lib/data";
+    // // filter data to get the project with the id that matches the id in the url
+    // PROJECTS_DATA.then(data => {
+    //     filtered_data = data.filter(project => project.short_url === $page.params.id );
+    //     filtered_data = filtered_data[0];
+    // });
 
 </script>
 
+{#if page_data}
+    <h1>{page_data.title}</h1>
+{/if}
+
+<!-- 
 <div class="project-container">
     {#if filtered_data}
         <h2 class="project-title">{filtered_data.name}</h2>
@@ -67,4 +83,4 @@
 
 </style>
 
-
+ -->
